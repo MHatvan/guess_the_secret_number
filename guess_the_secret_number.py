2c9a03b7073d4ab685917e42f9e49dcc29469629
MHatvan <markus_hatvan@aon.at>
2016-04-03 19:21:17 +0200

    first commit

---
+++ guess_the_secret_number.py
@@ -0,0 +1,6 @@
+secret_number = raw_input("Guess the secret number:")
+
+if secret_number == "23":
+    print("Congratulations, you are a mind reader!")
+else:
+    print("Sorry, you are wrong. Try again!")
